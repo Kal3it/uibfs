f8d4e4de3897b639e874c512b4ab52685f113feb
Javier Trivino <kal3it@gmail.com>
2017-06-04 10:21:10 +0200

    mi_cp permite ficheros dispersos

--- mi_cp.c
+++ mi_cp.c
@@ -10,6 +10,8 @@ int main(int argc, char const *argv[]){
     unsigned int tamBuffer = BLOCKSIZE;
     unsigned int offset = 0, bytesCopiados = 0;
     char buffer[tamBuffer];
+    char zerotest[tamBuffer];
+    memset(zerotest,0,tamBuffer);
     int respuesta;
 
     bmount(argv[1]);
@@ -31,17 +33,21 @@ int main(int argc, char const *argv[]){
     mi_creat(camino2,stat_camino1.permisos);
     if(respuesta < 0) return respuesta;
 
-    const char CENTINELA = 255;
+    const char CENTINELA = 1;
+    buffer[tamBuffer] = CENTINELA;
     memset(buffer,0,tamBuffer);
-    buffer[0] = CENTINELA; buffer[tamBuffer / 2] = CENTINELA; buffer[tamBuffer - 1] = CENTINELA;
     respuesta = mi_read(camino1, buffer, offset, tamBuffer);
     while(respuesta > 0){
-        if (buffer[0] != CENTINELA){ mi_write(camino2,buffer,offset,respuesta); }
+
+        // Comprobamos si el bloque contiene datos
+        if (memcmp(buffer,zerotest,tamBuffer)){
+            mi_write(camino2,buffer,offset,respuesta);
+            memset(buffer,0,tamBuffer);
+        }
         else printf("Bloque logico %u no inicializado, skipping\n",offset/tamBuffer);
+
         bytesCopiados += respuesta;
         offset += tamBuffer;
-        memset(buffer,0,tamBuffer);
-        buffer[0] = CENTINELA; buffer[tamBuffer / 2] = CENTINELA; buffer[tamBuffer - 1] = CENTINELA;
         respuesta = mi_read(camino1, buffer, offset, tamBuffer);
     }
 
