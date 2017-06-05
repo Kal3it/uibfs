c3e3068384a583da2d935ac2eb9a04cc84dfe170
Javier Trivino <kal3it@gmail.com>
2017-06-05 12:30:23 +0200

    Mejorada simulacion.c

--- simulacion.c
+++ simulacion.c
@@ -2,8 +2,10 @@
 #include <signal.h>
 #include "directorios/directorios.h"
 
-#define NPROCESOS  80
+#define NPROCESOS  100
 #define POS_MAX 50000
+#define SLEEP_PROCESS 0
+#define SLEEP_WRITE 0
 
 int acabados = 0;
 
@@ -12,7 +14,8 @@ void reaper(){
         acabados++;
 }
 
-void write_randomly(char *filepath){
+int write_randomly(char *filepath){
+    int resultado;
     struct registro reg;
     reg.pid = getpid();
     srand(time(NULL)+reg.pid);
@@ -22,30 +25,48 @@ void write_randomly(char *filepath){
         reg.time = time(NULL);
         reg.posicion=(rand()%POS_MAX)* sizeof(struct registro); // multiplo de sizeof(registro)
 
-        //fprintf(stderr,"%u escribe registro %u en posicion %u en el instante %lu\n",reg.pid,reg.nEscritura,reg.posicion,reg.time);
+//        printf("%u escribe registro %u en posicion %u en el instante %lu\n",reg.pid,reg.nEscritura,reg.posicion,reg.time);
 
-        mi_write(filepath, &reg, reg.posicion, sizeof(struct registro));
+        resultado = mi_write(filepath, &reg, reg.posicion, sizeof(struct registro));
+        if(resultado < 0){
+            return resultado;
+        }
 
-        usleep(2000);
+        usleep(SLEEP_WRITE);
     }
+
+    return 0;
 }
 
-void writer(int id, char *dir) {
+int writer(int id, char *dir) {
     char *filepath = (char *) malloc(100);
     sprintf(filepath,"%sproceso_%d/",dir,getpid());
+    int resultado;
 
-    mi_creat(filepath, 7);
-    //printf("%d ha creado el directorio '%s'\n", getpid(), filepath);
+    //printf("%d creando el directorio '%s'\n", getpid(), filepath);
+    resultado = mi_creat(filepath, 7);
+    if(resultado < 0){
+        return resultado;
+    }
+
     strcat(filepath,"prueba.dat");
-    mi_creat(filepath, 7);
-    //printf("%d ha creado el fichero '%s'\n", getpid(), filepath);
+//    printf("%d creando el fichero '%s'\n", getpid(), filepath);
+    resultado = mi_creat(filepath, 7);
+    if(resultado < 0){
+        return resultado;
+    }
 
-    write_randomly(filepath);
+    resultado = write_randomly(filepath);
+    if(resultado < 0){
+        return resultado;
+    }
 
     free(filepath);
+
+    return 0;
 }
 
-void forker(int id, char *dir) {
+void forker(char *fsdisk, int id, char *dir) {
     pid_t pid = fork();
 
     if (pid < 0)
@@ -55,11 +76,15 @@ void forker(int id, char *dir) {
     }
     else if (pid == 0)
     {
-        printf("Child %d lanzado\n",id);
+        bmount(fsdisk);
+//        printf("Child %d lanzado\n",id);
         writer(id, dir);
-        printf("Child %d terminado\n",id);
+//        printf("Child %d terminado\n",id);
+        bumount();
         exit(0);
     }
+
+    usleep(SLEEP_PROCESS);
 }
 
 int main (int argc, char *argv[]) {
@@ -72,27 +97,30 @@ int main (int argc, char *argv[]) {
     bmount(argv[1]);
 
     // Creamos el directorio de simulacion
-    char str_time[80];
     time_t now;
     time(&now);
     struct tm *ts = localtime(&now);
-    strftime(str_time, sizeof(str_time), "simul_%Y%m%d%H%M%S", ts); // todo SIZEOF?
+    char str_time[80];
+    strftime(str_time, 80, "simul_%Y%m%d%H%M%S", ts); // todo SIZEOF?
     char dir_simulacion[80];
     sprintf(dir_simulacion,"/%s/",str_time);
-    printf("%s\n",dir_simulacion);
     mi_creat(dir_simulacion,7);
-
-    // Lanzamos los procesos
-    for (int i = 0; i < NPROCESOS; ++i) {
-        forker(i, dir_simulacion);
-    }
-
-    wait(NULL);
-//    while (acabados < NPROCESOS){
-//        pause();
-//    }
-
     bumount();
 
-    return 0;
+    usleep(100000);
+    // Lanzamos los procesos
+    for (int i = 0; i < NPROCESOS; ++i) {
+        forker(argv[1], i, dir_simulacion);
+    }
+
+//    wait(NULL);
+    while (acabados < NPROCESOS){
+        pause();
+    }
+
+//    bumount();
+
+    printf("%s\n",dir_simulacion);
+
+    exit(0);
 }
